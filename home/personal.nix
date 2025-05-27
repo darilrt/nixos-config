190398face8c437b357eecdc27a2b81032136ad3
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-05-27 00:34:09 -0300

    update

--- home/personal.nix
+++ home/personal.nix
@@ -1,12 +1,26 @@
-{ pkgs, ... }:
-
-{
-  home.username = "daril";
-  home.homeDirectory = "/home/daril";
+{ pkgs, lib, ... }:
+let
+  workspace = "Personal";
+  username = "daril";
+in {
+  home.username = username;
+  home.homeDirectory = lib.mkDefault "/home/${username}/personal";
+  home.sessionVariables = {
+    WORKSPACE = workspace;
+  };
 
   home.packages = with pkgs; [
-    cowsay
-  ]; 
+    cowsay vim
+  ];
+
+  programs.home-manager.enable = true;
+  
+  programs.git = {
+    enable = true;
+    userName = "Daril Rodriguez";
+    userEmail = "darilrodriguez.2@gmail.com";
+    package = pkgs.git;
+  };
 
   home.stateVersion = "24.05";
 }
