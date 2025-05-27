91182ab4bdd1b75a6f0b7de35c26c03a45928a4f
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-05-27 02:54:12 -0300

    test

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.daril = {
-              shell = nixpkgs.zsh;
+            home-manager.users.daril = { pkgs, ... }: {
+              shell = pkgs.zsh;
               imports = [ ./home/personal.nix ];
             };
           }
