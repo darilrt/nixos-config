2345a3391a3a7815e1de2e821de904629765c4ba
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-05-27 02:05:37 -0300

    test

--- system/configuration.nix
+++ system/configuration.nix
@@ -38,14 +38,23 @@
     consoleLogLevel = 0;
     initrd.verbose = false;
     plymouth.enable = true;
-    kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
+    kernelParams = [
+      "quiet"
+      "splash"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "boot.shell_on_fail"
+    ];
 
     # Boot Loader
-    loader =
-    {
-      timeout = 0;
-      efi.canTouchEfiVariables = true;
-      systemd-boot.enable = true;
+    loader.grub = {
+      enable = true;
+      version = 2;
+      device = "/dev/sda";
+      useOSProber = true;
+      timeout = 0; # Timeout for the GRUB menu
+      silent = true; # Enable silent boot
     };
   };
 
