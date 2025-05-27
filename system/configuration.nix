e7f504ea6af527d935d558977e289dafea4b621b
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-05-27 02:13:06 -0300

    test

--- system/configuration.nix
+++ system/configuration.nix
@@ -13,40 +13,12 @@
   # Console settings
   console =
   {
-    font = "JetBrainsMono Nerd Font";
-    packages = with pkgs; [ nerdfonts ];
     keyMap = "us-acentos";
-  };
-
-  # TTY
-  fonts.fonts = with pkgs; [ meslo-lgs-nf ];
-  services.kmscon =
-  {
-    enable = true;
-    hwRender = true;
-    extraConfig =
-    ''
-      font-name=MesloLGS NF
-      font-size=14
-    '';
   };
 
   # Boot
   boot =
   {
-    # Plymouth
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    plymouth.enable = true;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-      "boot.shell_on_fail"
-    ];
-
     # Boot Loader
     loader.grub = {
       enable = true;
